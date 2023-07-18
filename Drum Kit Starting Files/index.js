b2940096453de043f1625125ae5b709d146c84cb
maunak <maunakdass@gmail.com>
2023-07-18 19:32:50 +0530

    drum 2

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,24 @@
+const audiow = () =>{
+    const audio = new Audio("sounds/crash.mp3")
+    audio.play();
+}
+const audioa = () =>{
+    const audio = new Audio("sounds/kick-bass.mp3")
+    audio.play();
+}
+const audios = () =>{
+    const audio = new Audio("sounds/snare.mp3")
+    audio.play();
+}
+const audiod = () =>{
+    const audio = new Audio("sounds/tom-1.mp3")
+    audio.play();
+}
+const audioj = () =>{
+    const audio = new Audio("sounds/tom-2.mp3")
+    audio.play();
+}
+const audiok = () =>{
+    const audio = new Audio("sounds/tom-3.mp3")
+    audio.play();
+}
